fa20dc2e8964a4092aa3aeb85d871eced4598cf8
Your Name <hirakhalil315@Gmail.com>
2023-12-27 18:31:57 +0500

    second commit

--- React/src/components/trainee/dashboard.jsx
+++ React/src/components/trainee/dashboard.jsx
@@ -25,7 +25,7 @@ function Dashboard() {
                     <section className="px-5 md:p-8 dark:bg-gray-800 dark:text-gray-100">
                         <div className="container grid grid-cols-1 gap-6 m-4 mx-auto md:m-0 md:grid-cols-2 xl:grid-cols-3">
                             <div className="bg-white  rounded-full flex overflow-hidden dark:bg-gray-900 dark:text-gray-100">
-                                <div className="flex flex-col items-center justify-center ml-auto h-8 w-8 rounded-full bg-indigo-500 text-white dark:text-gray-800">
+                                <div className="flex flex-col items-center justify-center ml-3 mt-3 h-8 w-8 rounded-full bg-indigo-500 text-white dark:text-gray-800">
 
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-6 h-6">
                                         <path d="M487.938,162.108l-224-128a16,16,0,0,0-15.876,0l-224,128a16,16,0,0,0,.382,28l224,120a16,16,0,0,0,15.112,0l224-120a16,16,0,0,0,.382-28ZM256,277.849,65.039,175.548,256,66.428l190.961,109.12Z"></path>
@@ -49,7 +49,7 @@ function Dashboard() {
                                 </div>
                             </div>
                             <div className="bg-white  rounded-full flex overflow-hidden dark:bg-gray-900 dark:text-gray-100">
-                                <div className=" flex items-center justify-center  px-4 rounded-full bg-indigo-500 text-white dark:text-gray-800">
+                                <div className="flex flex-col items-center justify-center ml-3 mt-3 h-8 w-8 rounded-full bg-indigo-500 text-white dark:text-gray-800">
                                     <BsMicrosoftTeams style={{ fontSize: '20px' }} />
                                 </div>
                                 <div className="flex items-center justify-between flex-1 p-3">
@@ -58,7 +58,7 @@ function Dashboard() {
                                 </div>
                             </div>
                             <div className="bg-white  rounded-full flex overflow-hidden dark:bg-gray-900 dark:text-gray-100">
-                                <div className=" flex items-center justify-center  px-4 rounded-full bg-indigo-500 text-white dark:text-gray-800">
+                                <div className="flex flex-col items-center justify-center ml-3 mt-3 h-8 w-8 rounded-full bg-indigo-500 text-white dark:text-gray-800">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-6 h-6">
                                         <path d="M256.25,16A240,240,0,0,0,88,84.977V16H56V144H184V112H106.287A208,208,0,0,1,256.25,48C370.8,48,464,141.2,464,255.75S370.8,463.5,256.25,463.5,48.5,370.3,48.5,255.75h-32A239.75,239.75,0,0,0,425.779,425.279,239.75,239.75,0,0,0,256.25,16Z"></path>
                                         <polygon points="240 111.951 239.465 288 368 288 368 256 271.563 256 272 112.049 240 111.951"></polygon>
@@ -70,7 +70,7 @@ function Dashboard() {
                                 </div>
                             </div>
                             <div className="bg-white  rounded-full flex overflow-hidden dark:bg-gray-900 dark:text-gray-100">
-                                <div className=" flex items-center justify-center  px-4 rounded-full bg-indigo-500 text-white dark:text-gray-800">
+                                <div className="flex flex-col items-center justify-center ml-3 mt-3 h-8 w-8 rounded-full bg-indigo-500 text-white dark:text-gray-800">
                                     <LiaProjectDiagramSolid style={{ fontSize: '20px' }} />
 
                                 </div>
@@ -80,7 +80,7 @@ function Dashboard() {
                                 </div>
                             </div>
                             <div className="bg-white  rounded-full flex overflow-hidden dark:bg-gray-900 dark:text-gray-100">
-                                <div className=" flex items-center justify-center  px-4 rounded-full bg-indigo-500 text-white dark:text-gray-800">
+                                <div className="flex flex-col items-center justify-center ml-3 mt-3 h-8 w-8 rounded-full bg-indigo-500 text-white dark:text-gray-800">
                                     <GrHelpBook style={{ fontSize: '20px' }} />
                                 </div>
                                 <div className="flex items-center justify-between flex-1 p-3">
